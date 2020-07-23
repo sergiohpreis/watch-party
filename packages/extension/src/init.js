d9a2e2509b90a494492020191d3b79cc34220608
Sergio Henrique Pereira Reis <sergio.hpreis@gmail.com>
2020-07-23 15:12:39 -0300

    refactor: removing logs

--- packages/extension/src/init.js
+++ packages/extension/src/init.js
@@ -3,8 +3,6 @@ import * as player from './player';
 import { getStreamService } from './utils/streamService';
 
 const start = () => {
-  const streamService = getStreamService();
-  console.log('Stream service', streamService);
   iframe.start(getStreamService());
   player.start(getStreamService());
 };
